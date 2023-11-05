71e7cbfccebb0cf869a00291dcd266007cace0f3
Marcel Gläser <marcelglaeser@users.noreply.github.com>
2023-11-05 11:29:21 +0100

    Update startup.py

--- files/startup.py
+++ files/startup.py
@@ -8,7 +8,7 @@ def cleanup_file(filename):
     """If file exists, delete it"""
     if os.path.isfile(filename):
         os.remove(filename)
-    else:  ## Show an error ##
+    else:
         print(f"{filename} does not exist")
 
 def copy_file_if_not_exists(src, dest):
@@ -21,68 +21,65 @@ def copy_file_if_not_exists(src, dest):
 
 def start_service(servicename):
     """Start a Linux service"""
-    print(f"startup service {servicename}")
+    print(f"Starting service {servicename}")
     p = subprocess.Popen(['/usr/sbin/service', servicename, 'start'], stdout=subprocess.PIPE)
     p.communicate()
     if p.returncode != 0:
-        print(f"startup of service {servicename} failed")
-
-def check_imap_configuration():
-    """ check if the IMAP accounts have already been configured"""
-    try:
-        with open("/root/accounts/imap_accounts.json", 'r') as f:
-            datastore = json.load(f)
-        # Loop through each account configuration
-        for account_name, account_config in datastore.items():
-            enabled = account_config.get("enabled", "False")
-            host = account_config.get("account", {}).get("server", "")
-            if enabled.lower() != "true":
-                print(f"ERROR: Antispambox configuration for {account_name} is not set to enabled - skipping this account")
-                continue
-            if host == "imap.example.net":
-                print(f"ERROR: No server configured for {account_name} in imap_accounts.json - please configure and restart")
-                continue
-            print(f"Configuration for {account_name} looks fine.")
-    except (IndexError, json.JSONDecodeError) as e:
-        print("ERROR: was not able to read imap_accounts.json.")
-        print(e)
-        sys.exit()
+        print(f"Failed to start service {servicename}")
 
 def fix_permissions():
-    """ fix the permissions of the rspamd and other necessary folders"""
-    # Removed the commented-out lines to clean up the script
-    permissions_paths = ['/etc/rspamd/local.d', '/var/spamassassin']
-    for path in permissions_paths:
+    """ fix the permissions of the bayes folders"""
+    paths_to_fix = ['/var/spamassassin/bayesdb', '/root/accounts']
+    for path in paths_to_fix:
         p = subprocess.Popen(['chmod', 'a+wr', path, '-R'], stdout=subprocess.PIPE)
-        (output, err) = p.communicate()
+        output, err = p.communicate()
         if p.returncode != 0:
-            print(f"chmod failed on {path}")
+            print(f"Failed to fix permissions for {path}")
             print(err)
             print(output)
 
-def download_spamassassin_rules():
-    """download the spamassassin rules"""
-    # This function has been simplified to call sa-update once
-    p = subprocess.Popen(['/usr/bin/sa-update', '--no-gpg', '-v'], stdout=subprocess.PIPE)
-    (output, err) = p.communicate()
-    if p.returncode not in (0, 1):
-        print("ERROR: sa-update failed")
-        print(err)
-        print(output)
-
-def start_imap_idle():
-    """Start the IMAP idle process for each account"""
-    # This function will need to be updated to handle multiple accounts
-    # Currently it assumes a single process handling all accounts
+def start_imap_idle(account):
+    """Start the IMAP idle process"""
+    print(f"Starting IMAP IDLE for {account['user']}")
     p = subprocess.Popen(['python3', '/root/antispambox.py'], stdout=subprocess.PIPE)
-    (output, err) = p.communicate()
-    # this will usually run endless
+    output, err = p.communicate()
     if p.returncode != 0:
-        print("ERROR: IMAPIDLE / PUSH / antispambox failed")
+        print(f"Failed IMAP IDLE for {account['user']}")
         print(err)
         print(output)
 
-# ... the rest of the script remains unchanged ...
+def check_imap_configuration():
+    """Check if the IMAP account has already been configured"""
+    try:
+        with open("/root/accounts/imap_accounts.json", 'r') as f:
+            datastore = json.load(f)
+        
+        if datastore["antispambox"]["enabled"].lower() != "true":
+            print("Antispambox is not enabled - exiting.")
+            sys.exit()
 
-print("\n\n *** start of IMAPIDLE / PUSH")
-start_imap_idle()
+        accounts = datastore["antispambox"]["accounts"]
+        for account in accounts:
+            if account["enabled"].lower() == "true":
+                print(f"Account {account['user']} is enabled.")
+                start_imap_idle(account)
+            else:
+                print(f"Account {account['user']} is disabled, skipping.")
+    except Exception as e:
+        print("ERROR: Unable to read imap_accounts.json.")
+        print(e)
+        sys.exit()
+
+print("\n\n ******* STARTUP ANTISPAMBOX ******* \n\n")
+
+cleanup_file("/root/.cache/irsd/lock")
+copy_file_if_not_exists("/root/imap_accounts.json", "/root/accounts/imap_accounts.json")
+fix_permissions()
+
+start_service("rsyslog")
+start_service("redis-server")
+start_service("rspamd")
+start_service("lighttpd")
+start_service("cron")
+
+check_imap_configuration()
